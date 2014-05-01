47fe496f1685d7773bfb6a734f1e1e80509fa6c7
Pieter Bogaerts <pieterbog@gmail.com>
2014-05-01 22:23:27 +0200

    sass added
    Added sass to the project.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,41 @@
 module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
-	  uglify: {
-	    build: {
-			files: {
-				'assets/js/build/plugins.js': ['assets/js/plugins/*.js']
+		uglify: {
+	    	build: {
+				files: {
+					'assets/js/build/plugins.js': ['assets/js/vendor/*.js','assets/js/plugins/*.js']
+				}
+			}
+	  	},
+	  	compass: {
+			dist: {
+				options: {
+				sassDir: 'assets/css/sass',
+				cssDir: 'assets/css'
+				}
+			}
+		},
+	  	sass: {
+			dist: {
+				files: {
+					'assets/css/style.css' : 'assets/css/sass/*.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['compass']
 			}
 		}
-	  }
 	});
 	
 	// Load the plugin that provides the "uglify" task.
 	  grunt.loadNpmTasks('grunt-contrib-uglify');
+	  grunt.loadNpmTasks('grunt-contrib-compass');
+	  grunt.loadNpmTasks('grunt-contrib-watch');
 	
 	  // Default task(s).
-	  grunt.registerTask('default', ['uglify']);
+	  grunt.registerTask('default', ['uglify', 'watch']);
 };
